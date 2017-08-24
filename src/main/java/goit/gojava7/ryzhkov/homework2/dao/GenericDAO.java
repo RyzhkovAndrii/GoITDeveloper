7190ad42dd38f897372485217f4ed7c0028f5e4b
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2017-08-24 18:43:24 +0300

    throws SQLException in all methods

--- src/main/java/goit/gojava7/ryzhkov/homework2/dao/GenericDAO.java
+++ src/main/java/goit/gojava7/ryzhkov/homework2/dao/GenericDAO.java
@@ -1,19 +1,20 @@
 package goit.gojava7.ryzhkov.homework2.dao;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 public interface GenericDAO<T, ID> {
 
-    void save(T entity);
+    void save(T entity) throws SQLException;
 
-    T getById(ID id);
+    T getById(ID id) throws SQLException;
 
-    Collection<T> getAll();
+    Collection<T> getAll() throws SQLException;
 
-    void update(T entity);
+    void update(T entity) throws SQLException;
 
-    void remove(T entity);
+    void remove(T entity) throws SQLException;
 
-    void removeAll();
+    void removeAll() throws SQLException;
 
 }
