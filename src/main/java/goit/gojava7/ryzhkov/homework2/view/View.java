8ef4f5e55b38174c7087697771e4318af10f0831
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2017-09-03 01:15:27 +0300

    generic interface was added

---
+++ src/main/java/goit/gojava7/ryzhkov/homework2/view/View.java
@@ -0,0 +1,15 @@
+package goit.gojava7.ryzhkov.homework2.view;
+
+public interface View {
+
+    void create();
+
+    void getById();
+
+    void getAll();
+
+    void update();
+
+    void removeById();
+
+}
